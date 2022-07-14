f777a7c43ce7da89c25275e2ec0acb58472d3427
Axel Broeglin <axelbroeglin@gmail.com>
2022-07-14 22:29:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,3 @@
-
-// const moviesList = document.getElementById('charactersList');
-// let movies = [];
-
-// const loadMovies = async () => {
-//     try {
-//         const res = await fetch('https://api.themoviedb.org/3/movie/550?api_key=6306a5921402700a1b44ea0634197368');
-//         movies = await res.json();
-//         displayMovies(movies);
-//         console.log(movies);
-//     } catch (err) {
-//         console.error(err);
-//     }
-// };
-
-// const displayMovies = (films) => {
-//     const htmlString = Array.from(films)
-//         .map((film) => {
-//             return `
-//             <li class="character">
-//                 <h2>${film.name}</h2>
-//                 <p>House: ${film.genres.name}</p>
-//             </li>
-//         `;
-//         })
-//         .join('');
-//     moviesList.innerHTML = htmlString;
-// };
-
-// loadMovies();
-
-// const searchBar = document.getElementById("searchBar");
-
-// searchBar.addEventListener("keydown", e => {
-//   const searchString = e.target.value.toLowerCase();
-//   const filteredCharacters = movies.filter(film => {
-//     return (
-//       film.name.toLowerCase().includes(searchString) ||
-//       film.genres.names.toLowerCase().includes(searchString)
-//     );
-//   });
-//   displayMovies(filteredCharacters);
-// });
-
-
 const API_KEY = 'api_key=6306a5921402700a1b44ea0634197368';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
@@ -132,14 +87,3 @@ form.addEventListener("keyup", e => {
     }
 
 });
-
-// searchBar.addEventListener("keydown", e => {
-//   const searchString = e.target.value.toLowerCase();
-//   const filteredCharacters = movies.filter(film => {
-//     return (
-//       film.name.toLowerCase().includes(searchString) ||
-//       film.genres.names.toLowerCase().includes(searchString)
-//     );
-//   });
-//   displayMovies(filteredCharacters);
-// });
